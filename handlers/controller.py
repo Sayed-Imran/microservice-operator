67f3a89ff1af710274209a178b1139ae60130275
Sayed-Imran <sayedimran00786@gmail.com>
2024-02-22 11:08:50 +0530

    Remove KubernetesController class from py_kube_controller.py

--- handlers/controller.py
+++ handlers/controller.py
@@ -1,178 +1,209 @@
-from kubernetes import client
-from schemas import DeployConfig
+import pykube
+from config import EnvConfig
+from custom_resources import GatewayResource
+from schemas import (
+    DeployConfig,
+    ServiceConfig,
+    VirtualServiceConfig,
+    VirtualServiceResource,
+)
 
 
 class KubernetesController:
-    def create_deployment(self, deploy_config: DeployConfig, apps_v1: client.AppsV1Api):
-        try:
-            body = client.V1Deployment(
-                metadata=client.V1ObjectMeta(
-                    name=deploy_config.name,
-                    namespace=deploy_config.namespace,
-                    labels=deploy_config.labels,
-                    annotations=deploy_config.annotations,
-                ),
-                spec=client.V1DeploymentSpec(
-                    replicas=deploy_config.replicas,
-                    selector=client.V1LabelSelector(match_labels=deploy_config.labels),
-                    template=client.V1PodTemplateSpec(
-                        metadata=client.V1ObjectMeta(
-                            labels=deploy_config.labels,
-                            annotations=deploy_config.annotations,
-                        ),
-                        spec=client.V1PodSpec(
-                            containers=[
-                                client.V1Container(
-                                    name=deploy_config.name,
-                                    image=deploy_config.image,
-                                    env=[
-                                        client.V1EnvVar(name=key, value=str(value))
-                                        for key, value in deploy_config.env.items()
-                                    ],
-                                    ports=[
-                                        client.V1ContainerPort(
-                                            container_port=deploy_config.port
-                                        )
-                                    ],
-                                    resources=client.V1ResourceRequirements(
-                                        limits=deploy_config.resources,
-                                        requests=deploy_config.resources,
-                                    ),
-                                )
-                            ],
-                            node_selector=deploy_config.node_selector,
-                            affinity=deploy_config.affinity,
-                            tolerations=deploy_config.tolerations,
-                            service_account=deploy_config.service_account,
-                        ),
-                    ),
-                ),
-            )
-            return apps_v1.create_namespaced_deployment(
-                namespace=deploy_config.namespace,
-                body=body,
-            )
-        except client.exceptions.ApiException as e:
-            if e.status == 409:
-                return apps_v1.replace_namespaced_deployment(
-                    name=deploy_config.name,
-                    namespace=deploy_config.namespace,
-                    body=body,
-                )
-            else:
-                raise e
+    def __init__(self) -> None:
+        if EnvConfig.ENV in ["dev", "test"]:
+            self.api = pykube.HTTPClient(pykube.KubeConfig.from_file("config.yml"))
+        else:
+            self.api = pykube.HTTPClient(pykube.KubeConfig.from_service_account())
 
-    def create_service(self, deploy_config: DeployConfig, core_v1: client.CoreV1Api):
-        try:
-            body = client.V1Service(
-                metadata=client.V1ObjectMeta(
-                    name=deploy_config.name,
-                    namespace=deploy_config.namespace,
-                    labels=deploy_config.labels,
-                    annotations=deploy_config.annotations,
-                ),
-                spec=client.V1ServiceSpec(
-                    selector=deploy_config.labels,
-                    ports=[
-                        client.V1ServicePort(
-                            port=deploy_config.port,
-                            target_port=deploy_config.port,
-                        )
-                    ],
-                ),
-            )
-            return core_v1.create_namespaced_service(
-                namespace=deploy_config.namespace,
-                body=body,
-            )
-        except client.exceptions.ApiException as e:
-            if e.status == 409:
-                return core_v1.replace_namespaced_service(
-                    name=deploy_config.name,
-                    namespace=deploy_config.namespace,
-                    body=body,
-                )
-            else:
-                raise e
+    def get_api(self):
+        return self.api
 
-    def delete_deployment(self, name: str, namespace: str, apps_v1: client.AppsV1Api):
-        return apps_v1.delete_namespaced_deployment(
-            name=name,
-            namespace=namespace,
-            body=client.V1DeleteOptions(
-                propagation_policy="Foreground",
-                grace_period_seconds=5,
-            ),
-        )
-
-    def delete_service(self, name: str, namespace: str, core_v1: client.CoreV1Api):
-        return core_v1.delete_namespaced_service(
-            name=name,
-            namespace=namespace,
-            body=client.V1DeleteOptions(
-                propagation_policy="Foreground",
-                grace_period_seconds=5,
-            ),
-        )
-
-    def get_deployment(self, name: str, namespace: str, apps_v1: client.AppsV1Api):
-        return apps_v1.read_namespaced_deployment(
-            name=name,
-            namespace=namespace,
-        )
-
-    def get_service(self, name: str, namespace: str, core_v1: client.CoreV1Api):
-        return core_v1.read_namespaced_service(
-            name=name,
-            namespace=namespace,
-        )
-
-    def update_deployment(self, deploy_config: DeployConfig, apps_v1: client.AppsV1Api):
-        body = client.V1Deployment(
-            metadata=client.V1ObjectMeta(
-                name=deploy_config.name,
-                namespace=deploy_config.namespace,
-                labels=deploy_config.labels,
-                annotations=deploy_config.annotations,
-            ),
-            spec=client.V1DeploymentSpec(
-                replicas=deploy_config.replicas,
-                selector=client.V1LabelSelector(match_labels=deploy_config.labels),
-                template=client.V1PodTemplateSpec(
-                    metadata=client.V1ObjectMeta(
-                        labels=deploy_config.labels,
-                        annotations=deploy_config.annotations,
-                    ),
-                    spec=client.V1PodSpec(
-                        containers=[
-                            client.V1Container(
-                                name=deploy_config.name,
-                                image=deploy_config.image,
-                                env=[
-                                    client.V1EnvVar(name=key, value=str(value))
-                                    for key, value in deploy_config.env.items()
-                                ],
-                                ports=[
-                                    client.V1ContainerPort(
-                                        container_port=deploy_config.port
-                                    )
-                                ],
-                                resources=client.V1ResourceRequirements(
-                                    limits=deploy_config.resources,
-                                    requests=deploy_config.resources,
-                                ),
-                            )
+    def create_deployment(self, deploy_config: DeployConfig, name: str):
+        deployment = {
+            "apiVersion": "apps/v1",
+            "kind": "Deployment",
+            "metadata": {
+                "labels": deploy_config.labels,
+            },
+            "spec": {
+                "replicas": deploy_config.replicas,
+                "selector": {
+                    "matchLabels": deploy_config.labels,
+                },
+                "template": {
+                    "metadata": {
+                        "labels": deploy_config.labels,
+                        "annotations": deploy_config.annotations,
+                    },
+                    "spec": {
+                        "containers": [
+                            {
+                                "name": name,
+                                "image": deploy_config.image,
+                                "ports": [{"containerPort": deploy_config.port}],
+                                "env": deploy_config.env,
+                                "resources": {
+                                    "limits": {
+                                        "cpu": deploy_config.resources.cpu_limit,
+                                        "memory": deploy_config.resources.memory_limit,
+                                    },
+                                    "requests": {
+                                        "cpu": deploy_config.resources.cpu_request,
+                                        "memory": deploy_config.resources.memory_request,
+                                    },
+                                },
+                            }
                         ],
-                        node_selector=deploy_config.node_selector,
-                        affinity=deploy_config.affinity,
-                        tolerations=deploy_config.tolerations,
-                        service_account=deploy_config.service_account,
-                    ),
-                ),
-            ),
+                    },
+                },
+            },
+        }
+
+        return deployment
+
+    def create_service(self, service_config: ServiceConfig):
+        service = {
+            "apiVersion": "v1",
+            "kind": "Service",
+            "metadata": {
+                "labels": service_config.labels,
+            },
+            "spec": {
+                "ports": [
+                    {
+                        "name": "port",
+                        "port": service_config.port,
+                        "targetPort": service_config.port,
+                    }
+                ],
+                "selector": service_config.labels,
+                "type": service_config.type,
+            },
+        }
+        return service
+
+    def create_virtual_service(self, virtual_service_config: VirtualServiceConfig):
+        service_details = self.get_service_by_labels(
+            virtual_service_config.labels, namespace=virtual_service_config.namespace
         )
-        return apps_v1.replace_namespaced_deployment(
-            name=deploy_config.name,
-            namespace=deploy_config.namespace,
-            body=body,
+        virtual_service = {
+            "apiVersion": "networking.istio.io/v1alpha3",
+            "kind": "VirtualService",
+            "metadata": {
+                "labels": virtual_service_config.labels,
+            },
+            "spec": {
+                "hosts": ["*"],
+                "gateways": [virtual_service_config.gateway],
+                "http": [
+                    {
+                        "match": [
+                            {
+                                "uri": {
+                                    "prefix": virtual_service_config.path,
+                                }
+                            }
+                        ],
+                        "rewrite": {
+                            "uri": "/",
+                        },
+                        "route": [
+                            {
+                                "destination": {
+                                    "host": f"{service_details.name}.{service_details.namespace}.svc.cluster.local",
+                                    "port": {
+                                        "number": virtual_service_config.port,
+                                    },
+                                },
+                                "timeout": virtual_service_config.timeout,
+                            }
+                        ],
+                    }
+                ],
+            },
+        }
+        return virtual_service
+
+    def update_deployment(self, deploy_config: DeployConfig):
+        deployment = self.get_deployment_by_labels(
+            deploy_config.labels, namespace=deploy_config.namespace
         )
+        deployment.obj["spec"]["replicas"] = deploy_config.replicas
+        deployment.obj["spec"]["template"]["spec"]["containers"][0][
+            "image"
+        ] = deploy_config.image
+        deployment.obj["spec"]["template"]["spec"]["containers"][0][
+            "env"
+        ] = deploy_config.env
+        deployment.obj["spec"]["template"]["spec"]["containers"][0]["resources"] = {
+            "limits": {
+                "cpu": deploy_config.resources.cpu_limit,
+                "memory": deploy_config.resources.memory_limit,
+            },
+            "requests": {
+                "cpu": deploy_config.resources.cpu_request,
+                "memory": deploy_config.resources.memory_request,
+            },
+        }
+        deployment.update()
+        return deployment
+
+    def update_service(self, service_config: ServiceConfig):
+        service = self.get_service_by_labels(
+            service_config.labels, namespace=service_config.namespace
+        )
+        service.obj["spec"]["ports"][0]["port"] = service_config.port
+        service.obj["spec"]["ports"][0]["targetPort"] = service_config.port
+        service.update()
+        return service
+
+    def update_virtual_service(self, virtual_service_config: VirtualServiceConfig):
+        virtual_service = self.get_virtual_service_by_labels(
+            virtual_service_config.labels, namespace=virtual_service_config.namespace
+        )
+        virtual_service.obj["spec"]["gateways"] = [virtual_service_config.gateway]
+        virtual_service.obj["spec"]["http"][0]["match"][0]["uri"][
+            "prefix"
+        ] = virtual_service_config.path
+        virtual_service.obj["spec"]["http"][0]["route"][0]["destination"]["port"][
+            "number"
+        ] = virtual_service_config.port
+        virtual_service.obj["spec"]["http"][0][
+            "timeout"
+        ] = virtual_service_config.timeout
+        virtual_service.update()
+        return virtual_service
+
+    def get_deployment_by_labels(self, labels: dict, namespace: str = "default"):
+        deployments = pykube.Deployment.objects(self.api).filter(
+            selector=labels, namespace=namespace
+        )
+        for deployment in deployments:
+            return deployment
+
+    def get_service_by_labels(self, labels: dict, namespace: str = "default"):
+        services = pykube.Service.objects(self.api).filter(
+            selector=labels, namespace=namespace
+        )
+        for service in services:
+            return service
+
+    def get_virtual_service_by_labels(self, labels: dict, namespace: str = "default"):
+        virtual_services = VirtualServiceResource.objects(self.api).filter(
+            selector=labels, namespace=namespace
+        )
+        for virtual_service in virtual_services:
+            return virtual_service
+
+    def get_gateway_by_name(self, name: str, namespace: str = "default"):
+        gateways = GatewayResource.objects(self.api).filter(
+            selector={"name": name}, namespace=namespace
+        )
+        for gateway in gateways:
+            return gateway
+
+    def create_gateway(self, gateway_config: dict):
+        gateway = GatewayResource(self.api, gateway_config)
+        gateway.create()
+        return gateway
